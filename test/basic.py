6d20ac833777874dc720bc6acb3d5c37629b639e
Einar Elén <einar.elen@gmail.com>
2022-01-27 10:00:29 +0100

    Update basic.py test

--- test/basic.py
+++ test/basic.py
@@ -6,14 +6,14 @@ p.maxEvents = 10
 # we want to see every event
 p.logFrequency = 1
 p.termLogLevel = 0
+# Set a run number
+p.run = 9001
 # we also only have an output file
 p.outputFiles = [ "justSim_" + str(p.maxEvents) + "_events.root" ]
 from LDMX.SimCore import simulator as sim
 from LDMX.Ecal import EcalGeometry
 mySim = sim.simulator( "mySim" )
 mySim.setDetector( 'ldmx-det-v12' )
-# Set a run number
-mySim.runNumber = 9001
 # Get a pre-written generator
 from LDMX.SimCore import generators as gen
 mySim.generators.append( gen.single_4gev_e_upstream_tagger() )
