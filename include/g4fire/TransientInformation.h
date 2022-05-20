62b9aa2e5fc696aa6027e7fd16d67841542b67d1
tomeichlersmith <eichl008@umn.edu>
2022-05-20 15:42:22 -0500

    move transient info classes into one basic type

---
+++ include/g4fire/TransientInformation.h
@@ -0,0 +1,55 @@
+#ifndef G4FIRE_TRANSIENTINFORMATION_H
+#define G4FIRE_TRANSIENTINFORMATION_H
+
+#include <fire/config/Parameters.h>
+
+#include <G4VUserEventInformation.hh>
+
+namespace g4fire {
+
+/**
+ * Geant4 has several different classes that are all
+ * ways of storing data about different objects in memory
+ * during the simulation of an event.
+ *
+ * This class encapsulates data that is only used during
+ * the simulation of an event. It has the exact same implementation
+ * as fire::config::Parameters, but having a different name for
+ * it is helpful for developers.
+ */
+using TransientInformation = ::fire::config::Parameters;
+
+/**
+ * The event information is transient information about the entire event
+ */
+class EventInformation : public G4VUserEventInformation, public TransientInformation {};
+
+/**
+ * The region information is transient information about regions
+ */
+class RegionInformation : public G4VUserRegionInformation, public TransientInformation {};
+
+/**
+ * The track information is transient information about tracks
+ */
+class TrackInformation : public G4VUserTrackInformation, public TransientInformation {
+ public:
+  /**
+   * Helper function to get the transient information about a track
+   */
+  static TrackInformation* get(const G4Track* t) {
+    if (not t->GetUserInformation()) {
+      const_cast<G4Track*>(t)->SetUserInformation(new TrackInformation);
+    }
+    return dynamic_cast<TrackInformation*>(t->GetUserInformation);
+  }
+};
+
+/**
+ * The primary particle information is transient information about primary particles
+ */
+class PrimaryParticleInformation : public G4VUserPrimaryParticleInformation, public TransientInformation {};
+
+}
+
+#endif
