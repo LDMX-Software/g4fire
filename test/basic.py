8a253beb4a54ea39678759d88dd7b4b14d2e0f52
tomeichlersmith <eichl008@umn.edu>
2022-08-19 15:49:06 -0500

    draft sim config as a target

--- test/basic.py
+++ test/basic.py
@@ -1,24 +1,16 @@
-from LDMX.Framework import ldmxcfg
-# create my process object
-p = ldmxcfg.Process( "test" )
-# how many events to process?
-p.maxEvents = 10
-# we want to see every event
-p.logFrequency = 1
-p.termLogLevel = 0
-# we also only have an output file
-p.outputFiles = [ "justSim_" + str(p.maxEvents) + "_events.root" ]
-from LDMX.g4fire import simulator as sim
-from LDMX.Ecal import EcalGeometry
-mySim = sim.simulator( "mySim" )
-mySim.setDetector( 'ldmx-det-v12' )
-# Set a run number
-mySim.runNumber = 9001
-# Get a pre-written generator
-from LDMX.g4fire import generators as gen
-mySim.generators.append( gen.single_4gev_e_upstream_tagger() )
-# add your configured simulation to the sequence
-mySim.description = 'Basic test Simulation'
-mySim.randomSeeds = [ 1 , 2 ]
 
-p.sequence.append( mySim )
+import fire.cfg
+p = fire.cfg.Process('sim')
+p.event_limit = 10
+p.log_frequency = 1
+p.term_level = 0
+p.output_file = f'basic_sim_{p.event_limit}_events.h5'
+p.run = 1
+
+from g4fire import simulator, generators, detector
+sim = simulator('basic')
+sim.description = 'Basic test simulation'
+sim.detector_constructor = detector.SimplePrism(5,5,5,'lead')
+sim.generators.append(generators.gun())
+
+p.sequence = [ sim ]
